a34302b86be623bcc500156ebc3eb86b3be63f15
caijf <369756941@qq.com>
2023-05-15 12:49:32 +0800

    build: 构建配置调整

---
+++ rollup.config.mjs
@@ -0,0 +1,26 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import { defineConfig } from 'rollup';
+
+export default defineConfig({
+  input: "src/index.tsx",
+  external: ["react", "tslib", "countdown-pro"],
+  output: [
+    {
+      format: "es",
+      file: "es/index.js"
+    },
+    {
+      format: "cjs",
+      file: "lib/index.js"
+    },
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({
+      tsconfig: "./tsconfig.build.json"
+    })
+  ]
+});
